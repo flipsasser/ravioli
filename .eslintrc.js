a5105eb349d354f7ac5198ab158b633e6e205676
Flip Sasser <flip.sasser@gmail.com>
2021-02-01 21:15:20 -0800

    WIP start to move the ts library over

---
+++ .eslintrc.js
@@ -0,0 +1,185 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:mocha/recommended",
+  ],
+  ignorePatterns: ["node_modules/"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    createDefaultProgram: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["import", "prettier", "sort-keys-shorthand"],
+  rules: {
+    /* TypeScript formatting */
+    "@typescript-eslint/explicit-function-return-type": "off", // TypeScript infers this stuff well
+    "@typescript-eslint/member-ordering": [
+      "error",
+      {
+        default: [
+          "public-static-field",
+          "protected-static-field",
+          "private-static-field",
+          "static-field",
+          "public-static-method",
+          "protected-static-method",
+          "private-static-method",
+          "static-method",
+          "public-instance-field",
+          "protected-instance-field",
+          "private-instance-field",
+          "public-field",
+          "protected-field",
+          "private-field",
+          "instance-field",
+          "field",
+          "constructor",
+          "public-instance-method",
+          "protected-instance-method",
+          "private-instance-method",
+          "public-method",
+          "protected-method",
+          "private-method",
+          "instance-method",
+          "method",
+        ],
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off", // TODO: Investigate re-enabling; this would be tricky
+    "@typescript-eslint/no-namespace": "off", // TODO: Not clear on why this is a thing but it messes with our extended enums
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+        vars: "all",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/prefer-optional-chain": "error",
+
+    /* Standard ESLint rules */
+    "arrow-parens": ["error", "as-needed"],
+    "comma-dangle": ["error", "always-multiline"],
+    "max-classes-per-file": "off", // Component and ComponentItem support
+    "no-console": [
+      "error",
+      {
+        allow: ["error"],
+      },
+    ],
+    "no-multiple-empty-lines": "error",
+    "no-unused-vars": "off", // TypeScript brings its own
+    "object-shorthand": ["error", "always"],
+    "prefer-const": [
+      "error",
+      {
+        destructuring: "all",
+      },
+    ],
+    "quote-props": ["error", "as-needed"],
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+      },
+    ],
+    semi: ["error", "never"],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+      },
+    ],
+    "sort-keys": "off", // We use a custom sorter that enforces shorthand-first keys
+
+    /* import/export rules */
+    "import/default": "off", // Let TypeScript sort this out
+    "import/first": "error", // Use custom import/order grouping rather than enforcing a single group
+    "import/export": "error",
+    "import/named": "off", // Let TypeScript sort this out
+    "import/namespace": "error",
+    "import/no-default-export": "error",
+    "import/no-duplicates": "error",
+    "import/no-extraneous-dependencies": "off",
+    // TODO: Restore this - trouble is, it's claiming teller-core is an extraneous dependency
+    // because it's defined through tsconfig paths
+    // "import/no-extraneous-dependencies": [
+    //   "error",
+    //   {
+    //     packageDir: [
+    //       __dirname,
+    //       join(__dirname, "client", "core"),
+    //       join(__dirname, "client", "web"),
+    //     ],
+    //   },
+    // ],
+    "import/no-named-as-default": "error",
+    "import/no-named-as-default-member": "error",
+    "import/no-unresolved": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: "asc",
+        },
+        groups: [["builtin", "external"], "internal", "index", "sibling", "parent"],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            group: "internal",
+            pattern: "src/*",
+          },
+        ],
+      },
+    ],
+
+    /* Mocha rules */
+    "mocha/no-mocha-arrows": "off",
+
+    /* Finally, miscellaneous */
+    "sort-keys-shorthand/sort-keys-shorthand": [
+      "error",
+      "asc",
+      {
+        caseSensitive: true,
+        natural: false,
+        minKeys: 2,
+        shorthand: "first",
+      },
+    ],
+    "prettier/prettier": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        directory: ["./tsconfig.json"],
+      },
+    },
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,154 @@
+import { createCipheriv } from "crypto"
+import deepmerge from "deepmerge"
+import { existsSync, readFileSync } from "fs"
+import { load as loadYAML } from "js-yaml"
+import { resolve } from "path"
+
+interface LoadOptions {
+  envKey?: string
+  keyPath?: string
+}
+
+function resolveCredentialPath(path: string) {
+  return resolve(__dirname, "../../../..", path)
+}
+
+function loadCredentials(file: string, options: LoadOptions): any {
+  // Ensure the file exists and parse it if we can
+  const path = resolveCredentialPath(file)
+  if (!existsSync(path)) {
+    return {}
+  }
+
+  // Determine which key to use - either an ENV variable or a file - and generate a secret from it
+  const { envKey, keyPath } = options
+  let key = envKey ? process.env[envKey] || process.env[`RAILS_${envKey}`] : null
+  if (!key) {
+    const keyFile = resolveCredentialPath(keyPath)
+    if (!existsSync(keyFile)) {
+      return {}
+    }
+
+    key = readFileSync(keyFile, "ascii")
+  }
+
+  const secret = Buffer.from(key, "hex")
+
+  // Read the encrypted file and split it into its constituent parts
+  const contents = readFileSync(path, "ascii")
+  const [data, iv, _] = contents.split("--").map(part => Buffer.from(part, "base64"))
+
+  // Decrypt the file
+  const cipher = createCipheriv("aes-128-gcm", secret, iv)
+  let decrypted = Buffer.concat([cipher.update(data), cipher.final()])
+
+  // Unmarshal a marshal'd Ruby string. The delimiter of a marshal'd string is a single '"'
+  // character, so we pull everything between those two
+  decrypted = decrypted.slice(decrypted.indexOf('"') + 1)
+  const readByte = () => {
+    const nextByte = decrypted[0]
+    decrypted = decrypted.slice(1)
+    return nextByte
+  }
+
+  // The next byte will indicate the length of the subsequent string
+  const pointer = readByte()
+
+  // If it's between 4 and 127, that's the length of the substring - otherwise it indicates the
+  // number of bytes to
+  // consume in calculating the actual length
+  const consume = pointer >= 1 && pointer <= 3 ? pointer : 0
+  let length = consume > 0 ? 0 : pointer
+
+  // If it's between 1 and 3 (inclusive), we consume that number of subsequent bytes and sum them to
+  // determine string length
+  for (let i = 0; i < consume; i++) {
+    length = length | (readByte() << (8 * i))
+  }
+  decrypted = decrypted.slice(0, length)
+
+  // Neato - parse the YAML
+  return loadYAML(decrypted.toString()) || {}
+}
+
+interface BuildCombinatorOptions {
+  transformKey?: (keys: string[]) => string
+  transformObject?: (parent: any, child: any, keys: string[]) => any
+  transformValue?: (value: any, keys: string[]) => any
+}
+
+type Combinator<T = any> = (object: any, key?: string[]) => T
+
+function buildCombinator<T = any>({
+  transformKey = keys => keys[keys.length - 1],
+  transformObject = object => object, // Do nothing to child objects
+  transformValue = value => value,
+}: BuildCombinatorOptions = {}): Combinator<T> {
+  const combinator = (object: any, keys: string[] = []): T => {
+    let combinedObject: any = {}
+    for (const key in object) {
+      if (object.hasOwnProperty(key)) {
+        const value = object[key]
+        const newKeys = [...keys, key]
+
+        switch (typeof value) {
+          case "object":
+            combinedObject = transformObject(combinedObject, value, newKeys)
+            break
+          default:
+            const newKey = transformKey(newKeys)
+            combinedObject[newKey] = transformValue(value, newKeys)
+        }
+      }
+    }
+
+    return combinedObject
+  }
+
+  return combinator
+}
+
+const combineCredentialsWithEnv = buildCombinator({
+  transformObject: (parent, child, keys) => {
+    parent[keys[keys.length - 1]] = combineCredentialsWithEnv(child, keys)
+    return parent
+  },
+  transformValue: (value, keys) => process.env[keys.join("_").toUpperCase()] || value,
+})
+
+const aliasCredentials: Combinator = buildCombinator({
+  transformKey: keys => `process.env.${keys.join("_").toUpperCase()}`,
+  transformObject: (parent, child, keys) => ({
+    ...parent,
+    ...aliasCredentials(child, keys),
+  }),
+  transformValue: value => JSON.stringify(value),
+})
+
+const rootCredentials = loadCredentials("config/credentials.yml.enc", {
+  envKey: "ROOT_KEY",
+  keyPath: "config/master.key",
+})
+
+const env = process.env.NODE_ENV || "development"
+const envCredentials = loadCredentials(`config/credentials/${env}.yml.enc`, {
+  envKey: "MASTER_KEY",
+  keyPath: `config/credentials/${env}.key`,
+})
+
+const combinedCredentials = deepmerge<any>(rootCredentials, envCredentials)
+export const credentials: any = combineCredentialsWithEnv(combinedCredentials)
+export const aliasedCredentials = aliasCredentials(credentials)
+
+export function getCredential(...keyPath: string[]): void {
+  let result: any = credentials
+  for (const key of keyPath) {
+    if (!result || typeof result !== "object") {
+      return null
+    }
+
+    result = result[key]
+  }
+
+  return result
+}
