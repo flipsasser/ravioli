1384f36d7b2e03cff0607c2a87d5463c88dab689
Flip Sasser <flip.sasser@gmail.com>
2022-08-11 02:07:26 -0700

    Trap bad YAML parsing

--- src/parseYAML.ts
+++ src/parseYAML.ts
@@ -1,7 +1,12 @@
 import { parse } from "yaml"
 
 export function parseYAML(input: string): any {
-  return parse(input, {
-    merge: true,
-  })
+  try {
+    const parsed = parse(input, {
+      merge: true,
+    })
+    return parsed
+  } catch (_) {
+    return {}
+  }
 }
